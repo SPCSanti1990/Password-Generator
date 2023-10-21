9e3c8f7e5dd936dd71f95167cd02e6ac2d9bbc55
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2023-10-21 08:35:07 -0400

    added dialog box for user input on what characters to add into password

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,37 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  let validChar = "";
+  let lowerAlpha = ["abcdefghijklmnopqrstuvwxyz"];
+  let capitalAlpha = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  let number = ["0123456789"];
+  let specialChar = ["!@#$%^&*()<>?=+-"];
+
+  if (confirm("Use Lower Case Characters?")) {
+    validChar = validChar + lowerAlpha;
+  }
+  if (confirm("Use Upper Case Characters?")) {
+    validChar = validChar + capitalAlpha;
+  }
+  if (confirm("Use Numbers?")) {
+    validChar = validChar + number;
+  }
+  if (confirm("Use Special Characters?")) {
+    validChar = validChar + specialChar;
+  }
+  for (let i = 0; i < length; i++){
+    const randomPassword = Math.floor(Math.random() * validChar.length);
+  }
 }
 
 // Add event listener to generate button
