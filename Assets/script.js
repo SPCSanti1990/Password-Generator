cb279e5162d0ececabcc8015d5b4340ec97c441e
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2023-10-21 12:57:21 -0400

    random password generator function number check bug fixed

--- Assets/script.js
+++ Assets/script.js
@@ -9,29 +9,26 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+// Random password generator function
 function generatePassword() {
+  // Empty array to store user defined characters based on promt
   let validChar = Array();
+  // Arrays containing characters used in password generation
   let lowerAlpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   let capitalAlpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   let number = ["0","1","2","3","4","5","6","7","8","9"];
   let specialChar = ["!","@","#","$","%","^","&","*","(",")","<",">","?","=","+","-"];
+  // Variable used to store generated password
   let generatedPWD = "";
+  // Variable used to temporarly store picked character from generated array
   let randomPassword = "";
+  // Varaible to store password length
   let passwordLength = prompt("Your custom password should be between 8 and 128 characters long.");
   passwordLength = Number(passwordLength);
 
-  if (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt("Your custom password should be between 8 and 128 characters long.");
-    passwordLength = Number(passwordLength);
-  }
-
-  if (passwordLength == null) {
-    passwordLength = prompt("Your custom password should be between 8 and 128 characters long.")
-    passwordLength = Number(passwordLength);
-  }
-
-  else {
+  // Check to see if input is between 8 and 128
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    // Promt user to select what characters to use and add them to an array
     if (confirm("Use Lower Case Characters?")) {
       validChar = validChar.concat(lowerAlpha);
     }
@@ -44,12 +41,18 @@ function generatePassword() {
     if (confirm("Use Special Characters?")) {
       validChar = validChar.concat(specialChar);
     }
+    // Loop through created array, the number of times decided by user to randomly pick characters
     for (let i = 0; i < passwordLength; i++){
       randomPassword = validChar[Math.floor(Math.random() * validChar.length)];
       generatedPWD = generatedPWD + randomPassword;
     }
-  } return generatedPWD;
+    // Return generated password
+   return generatedPWD;
+  }
+  // Check to see if input is a number
+  else {
+  writePassword();
+  }
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
